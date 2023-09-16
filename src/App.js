887110578c2e63a190511127be564a1f78028946
Jensen-Jacob <jensenupwork@gmail.com>
2023-09-16 23:37:45 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  let day;
+
+  if (count > 0) {
+    if (count === 1) {
+      day = "day from today is";
+    } else {
+      day = "days from today is";
+    }
+  } else if (count < 0) {
+    if (count === -1) {
+      day = "day ago from today was";
+    } else {
+      day = "days ago from today was";
+    }
+  } else {
+    day = "Today is";
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div className="flex-container">
+      <div className="flex-item">
+        <div>
+          <p> Step: {step} </p>
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          />
+        </div>
+        {/* <div> */}
+        <div className="count-div">
+          <p> Count: </p>
+          <button onClick={() => setCount((c) => c - step)}>&minus;</button>
+          <input
+            type="text"
+            value={count}
+            onChange={(e) => setCount(Number(e.target.value))}
+          />
+          <button onClick={() => setCount((c) => c + step)}>&#43;</button>
+        </div>
+        {/* </div> */}
+        <div>
+          <p>
+            {count !== 0 && Math.abs(count)} {day} {date.toDateString()}
+          </p>
+          <button className="reset-btn" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
